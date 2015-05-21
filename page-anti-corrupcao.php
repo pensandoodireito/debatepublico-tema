6537465d6b250f2a5d268a4889d1aec3d9dbf48c
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-05-21 11:54:06 -0300

    Criação da página de Medidas de enfrentamento à corrupção e à impunidade; Criação das pastas, css e less, js e images, para a estrutura de debates (genérico) e debates específicos;

---
+++ page-anti-corrupcao.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+<div id="anti-corr">
+	<div class="anti-corr-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 text-center">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/anti-corrupcao/logo-anti-corrupcao.png" class="img-adptive" alt="Logo: Medidas de combate à corrupção e a impunidade ">
+				</div>
+				<div class="col-sm-6 col-md-4 white">
+					<h1 class="font-roboto h1 mt-md"><strong>Participe, opine, ajude!</strong></h1>
+					<p class="mt-md">As medidas de enfrentamento à impunidade e a corrupção são uma série de propostas de reformas legais com o objetivo de tornar mais eficiente o processo penal</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="anti-corr-oque">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2 class="font-roboto red">O que é?</h2>
+					<p class="mt-md">Muitas medidas voltadas à prevenção e ao enfrentamento à corrupção e à impunidade vem sendo implementadas, mas há contínua necessidade de aperfeiçoamento deste sistema anticorrupção.</p>
+					<p class="mt-md">Conforme as debilidades das estruturas são constatadas e são apresentadas soluções, coloca-se luz sobre novos desafios que precisam ser enfrentados.</p>
+				</div>
+				<div class="col-sm-4 text-center">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/anti-corrupcao/img-anti-corrupcao-001.png" class="img-adptive" alt="Logo: Medidas de combate à corrupção e a impunidade ">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="anti-corr-eixos">
+		<div class="tabs-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<!-- tabs left -->
+						<div class="tabs-left">
+							<ul class="nav nav-tabs">
+								<li><a href="#a" data-toggle="tab">One</a></li>
+								<li class="active"><a href="#b" data-toggle="tab">Two</a></li>
+								<li><a href="#c" data-toggle="tab">Twee</a></li>
+							</ul>
+							<div class="tab-content">
+								<div class="tab-pane active" id="a">Lorem ipsum dolor sit amet, charetra varius quam sit amet vulputate.
+								Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero.</div>
+								<div class="tab-pane" id="b">Secondo sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan.
+								Aliquam in felis sit amet augue.</div>
+								<div class="tab-pane" id="c">Thirdamuno, ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate.
+								Quisque mauris augue, molestie tincidunt condimentum vitae. </div>
+							</div>
+						</div>
+						<!-- /tabs -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
