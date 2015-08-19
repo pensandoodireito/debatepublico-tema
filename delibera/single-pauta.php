89fd50591914ed9682a83382fd68e649829f3417
Theoziran Lima <theoziran@gmail.com>
2015-08-19 00:00:43 -0300

    Adição de menu auxiliar na pauta

--- delibera/single-pauta.php
+++ delibera/single-pauta.php
@@ -5,6 +5,11 @@ $situacao = delibera_get_situacao(get_the_ID());
 
 <div class="conteudo" id="debatepublico-generic">
     <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <?php get_template_part('menu', 'horizontal'); ?>
+            </div>
+        </div>
         <div class="situacao-<?php echo $situacao->slug; ?> clearfix">
         <?php
 
